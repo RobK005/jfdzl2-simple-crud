27089e84e4b4512b7384fe3cb1ba5f066fc55ba0
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-02-23 11:57:54 +0100

    sending beers

--- src/Form.js
+++ src/Form.js
@@ -8,24 +8,35 @@ class Form extends Component {
     imageUrl: ''
   }
 
-  handleChangeName = (event) => {
+  handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = () => {
+    // POST https://beers-bunkier.firebaseapp.com/api/v1/beers
+    // data: this.state
+
+    fetch('https://beers-bunkier.firebaseapp.com/api/v1/beers', {
+      method: 'POST',
+      body: JSON.stringify(this.state)
+    });
+
   }
 
   render() {
     return (
       <div>
         <div>
-          Name: <input onChange={this.handleChange} type="text" />
+          Name: <input onChange={this.handleChange} type="text" name="name" />
         </div>
         <div>
-          Description: <input onChange={this.handleChange} type="text"  />
+          Description: <input onChange={this.handleChange} type="text" name="description" />
         </div>
         <div>
-          Image url: <input onChange={this.handleChange} type="text"  />
+          Image url: <input onChange={this.handleChange} type="text" name="imageUrl" />
         </div>
         <div>
-          <button>Send</button>
+          <button onClick={this.handleSubmit}>Send</button>
         </div>
       </div>
     );
