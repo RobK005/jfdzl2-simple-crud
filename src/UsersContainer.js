dd0bc099dcd3b7f92ccb42709d8b7654c1d0849b
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-02-23 10:54:28 +0100

    before random user

--- src/UsersContainer.js
+++ src/UsersContainer.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 
-const users = [{
-  id: 1,
-  name: 'Ala'
-}, {
-  id: 2,
-  name: 'Bogdan'
-}];
-
 class UsersContainer extends Component {
+
+  state = {
+    users: [{
+      id: 1,
+      name: 'Ala'
+    }, {
+      id: 2,
+      name: 'Bogdan'
+    }]
+  }
+
+  fetchUsers() {
+    fetch('https://randomuser.me/api/')
+    .then(response => {
+      console.log(response.status);
+      return response.json();
+    })
+    .then(data => {
+      this.setState({ users: data });
+    });
+  }
 
   render() {
     return(
       <div>
         <h1>My users:</h1>
-        {users.map(user => {
+        {this.state.users.map(user => {
           return <p key={`user-${user.id}`}>{user.name}</p>
         })}
       </div>
